337d433b4f1d39e480374e2f76a948c9f18638a3
Amber Liu <amberl@nvidia.com>
2025-03-15 08:30:57 +0000

    Fix XQA allowedHeadRatio for 8:1 / Qwen2.5 3B

--- cpp/attentionPlugin/xqa/decoderXQARunner.cpp
+++ cpp/attentionPlugin/xqa/decoderXQARunner.cpp
@@ -266,7 +266,7 @@ bool DecoderXQARunner::canImplement(int32_t numQHeads, int32_t numKVHeads, int32
 {
     bool const checkHeadNumbers = numQHeads % numKVHeads == 0;
     bool const checkType = dataType == DataType::kHALF;
-    std::vector<int> allowedHeadRatio{1, 3, 4, 6, 7};
+    std::vector<int> allowedHeadRatio{1, 3, 4, 6, 7, 8};
     bool const checkQHeadPerKV
         = std::find(allowedHeadRatio.begin(), allowedHeadRatio.end(), int(numQHeads / numKVHeads))
         != allowedHeadRatio.end();
