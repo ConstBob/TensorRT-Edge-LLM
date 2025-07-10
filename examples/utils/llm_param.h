f4b1f2061b18051d88169f48ab4be0ed6fbf3e33
Sunny Gai <qgai@nvidia.com>
2025-07-10 07:28:37 -0700

    fix bugs for llm export

--- examples/utils/llm_param.h
+++ examples/utils/llm_param.h
@@ -52,7 +52,7 @@ struct VLMBuildParams
     int64_t imageTokens{512};
     int64_t minImageTokens{4};
     int64_t maxImageTokens{1024};
-    bool usePromptTuning{true};
+    bool usePromptTuning{false};
 };
 
 struct VLMRunParams
