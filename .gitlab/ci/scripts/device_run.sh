9e3495bcbe72f22bdd8c3fa6afd935e69fb53621
Luxiao Zheng <luxiaoz@nvidia.com>
2025-07-29 21:32:19 -0700

    DOSLLM-106 Sanity CI Bringup

---
+++ .gitlab/ci/scripts/device_run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+echo "Running testing on device"
+
+board_password={BOARDPASSWORD}
+cd /home/nvidia/tensorrt-edge-llm
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/nvidia/tensorrt-edge-llm/TensorRT-jp6/lib
+mv build-jp6 build
+cd build
+./unitTest
+
+echo "Done running testing"
