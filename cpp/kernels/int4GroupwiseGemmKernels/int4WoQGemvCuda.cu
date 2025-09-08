e2839cb1211c969e5b0e7f8b77e02138babf1c6d
Luxiao Zheng <luxiaoz@nvidia.com>
2025-09-08 16:35:08 -0700

    None: Update license and create OSS files for all files

--- cpp/kernels/int4GroupwiseGemmKernels/int4WoQGemvCuda.cu
+++ cpp/kernels/int4GroupwiseGemmKernels/int4WoQGemvCuda.cu
@@ -18,9 +18,23 @@
  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
+
+ * SPDX-FileCopyrightText: Copyright (c) 2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+ * SPDX-License-Identifier: Apache-2.0
  *
- * Modification made by TensorRT Edge-LLM team to follow TensorRT's Int4 weights-only
- * quantization semantics.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * reference: https://github.com/mit-han-lab/llm-awq/blob/main/awq/kernels/csrc/quantization_new/gemv/gemv_cuda.cu
  */
 
 #include "dequantize.cuh"
